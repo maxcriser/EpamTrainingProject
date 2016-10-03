597f7b24ec6b9cf3c87f5ba815fc739a925eea9b
maxcriser <mv.maxcriser@gmail.com>
2016-10-03 15:37:45 +0300

    Lecture #3

--- QNote/app/src/main/java/com/maxcriser/qnote/MainActivity.java
+++ QNote/app/src/main/java/com/maxcriser/qnote/MainActivity.java
@@ -1,13 +1,26 @@
 package com.maxcriser.qnote;
 
+import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText; // ctrl+alt+f
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText2);  // ctrl+alt+f
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
     }
 }
---
+++ QNote/app/src/main/java/com/maxcriser/qnote/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.maxcriser.qnote;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by IstiN on 30.09.2016.
+ */
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
