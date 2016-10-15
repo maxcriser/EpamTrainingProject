77526a6d0d3899e5dec97c6b45f819cf3558f811
maxcriser <mv.maxcriser@gmail.com>
2016-10-15 18:57:22 +0300

    Fixed install apk problem

--- News/mobile/src/main/java/com/maxcriser/news/ui/HelloActivity.java
+++ News/mobile/src/main/java/com/maxcriser/news/ui/HelloActivity.java
@@ -6,13 +6,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.maxcriser.news.R;
+import com.maxcriser.news.ui.version.TitleVersion;
+import com.maxcriser.news.view.RobotoThinTextView;
 
 public class HelloActivity extends AppCompatActivity {
+
+    RobotoThinTextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello);
+
+        title = (com.maxcriser.news.view.RobotoThinTextView) findViewById(R.id.title_news);
+        title.setText(TitleVersion.setTitle());
     }
 
     public void onClick(View view) {
---
+++ News/mobile/src/main/java/com/maxcriser/news/ui/version/TitleVersion.java
@@ -0,0 +1,9 @@
+package com.maxcriser.news.ui.version;
+
+import com.maxcriser.news.BuildConfig;
+
+public class TitleVersion {
+    public static String setTitle() {
+        return BuildConfig.APP_TITLE_VERSION;
+    }
+}
