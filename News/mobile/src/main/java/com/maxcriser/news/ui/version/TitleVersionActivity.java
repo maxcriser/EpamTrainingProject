219960bb847d6888c091f483b37a8226fe0becab
maxcriser <mv.maxcriser@gmail.com>
2016-10-15 17:49:31 +0300

    Revert "HW 17.10.2016 #8"
    This reverts commit e693ebd0c4b73b03820ae6f07dde2938e418ae99.

--- News/mobile/src/main/java/com/maxcriser/news/ui/version/TitleVersionActivity.java
+++
@@ -1,9 +0,0 @@
-package com.maxcriser.news.ui.version;
-
-import com.maxcriser.news.BuildConfig;
-
-public class TitleVersionActivity {
-    public static String setTitle(){
-        return BuildConfig.APP_TITLE_VERSION;
-    }
-}
