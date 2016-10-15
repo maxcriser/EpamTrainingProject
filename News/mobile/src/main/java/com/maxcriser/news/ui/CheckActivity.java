43b5074d53f1a7363f9bb5e8ae12e789b6cbf353
maxcriser <mv.maxcriser@gmail.com>
2016-10-15 19:01:46 +0300

    Refactory code

--- News/mobile/src/main/java/com/maxcriser/news/ui/CheckActivity.java
+++ News/mobile/src/main/java/com/maxcriser/news/ui/CheckActivity.java
@@ -2,8 +2,8 @@ package com.maxcriser.news.ui;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class CheckActivity extends AppCompatActivity {
 
@@ -24,7 +24,7 @@ public class CheckActivity extends AppCompatActivity {
 //        if (FLAG_REG) {
 //            intent = new Intent(this, MainActivity.class);
 //        } else {
-            intent = new Intent(this, HelloActivity.class);
+        intent = new Intent(this, HelloActivity.class);
 //        }
 
         startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
--- News/mobile/src/main/java/com/maxcriser/news/ui/MainActivity.java
+++ News/mobile/src/main/java/com/maxcriser/news/ui/MainActivity.java
@@ -1,7 +1,7 @@
 package com.maxcriser.news.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.maxcriser.news.R;
 
