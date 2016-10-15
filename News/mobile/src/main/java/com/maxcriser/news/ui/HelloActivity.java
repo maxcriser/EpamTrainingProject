63e2fdf48c7a5577e9fe16fc5bbd60d1bba6ba6c
maxcriser <mv.maxcriser@gmail.com>
2016-10-15 17:48:37 +0300

    Revert "Edit hello+titile"
    This reverts commit 0ee1e34591c4aca010f6372f4c2b0e464e07037b.

--- News/mobile/src/main/java/com/maxcriser/news/ui/HelloActivity.java
+++ News/mobile/src/main/java/com/maxcriser/news/ui/HelloActivity.java
@@ -6,13 +6,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.maxcriser.news.R;
+import com.maxcriser.news.ui.version.TitleVersionActivity;
 
 public class HelloActivity extends AppCompatActivity {
+
+    com.maxcriser.news.view.RobotoThinTextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello);
+
+        title = (com.maxcriser.news.view.RobotoThinTextView) findViewById(R.id.title_news);
+        title.setText(TitleVersionActivity.setTitle());
     }
 
     public void onClick(View view) {
