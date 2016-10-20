b3a522a81839e3ea0d3f31d372dd2435fc700f25
maxcriser <mv.maxcriser@gmail.com>
2016-10-20 23:50:02 +0300

    comments

--- Cards/app/src/main/java/com/maxcriser/cards/ui/cards/ProtectedBankCards.java
+++ Cards/app/src/main/java/com/maxcriser/cards/ui/cards/ProtectedBankCards.java
@@ -1,23 +1,18 @@
 package com.maxcriser.cards.ui.cards;
 
-import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
 
 import com.maxcriser.cards.R;
 
-import java.io.IOException;
-
 
 public class ProtectedBankCards extends AppCompatActivity {
 
-    Camera camera;
-    SurfaceView preview;
+//    Camera camera;
+//    SurfaceView preview;
 
-    View btnAddNewCard;
+//    View btnAddNewCard;
 
 
     //TODO if this page is empty - fragment_empty_page.xml visibility VISIBLE
@@ -26,35 +21,35 @@ public class ProtectedBankCards extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_protected_bank_cards);
 
-        btnAddNewCard = findViewById(R.id.add_new_card);
-        btnAddNewCard.setOnClickListener(new onAddNewCardListener());
+//        btnAddNewCard = findViewById(R.id.add_new_card);
+//        btnAddNewCard.setOnClickListener(new onAddNewCardListener());
 
-        SurfaceHolder surfaceHolder;
-        surfaceHolder = preview.getHolder();
-        try {
-            camera.setPreviewDisplay(surfaceHolder);
-        } catch (IOException pE) {
-            pE.printStackTrace();
-        }
+//        SurfaceHolder surfaceHolder;
+//        surfaceHolder = preview.getHolder();
+//        try {
+//            camera.setPreviewDisplay(surfaceHolder);
+//        } catch (IOException pE) {
+//            pE.printStackTrace();
+//        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        camera = Camera.open();
+//        camera = Camera.open();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
 
-        if (camera != null) {
-            camera.setPreviewCallback(null);
-            camera.stopPreview();
-            camera.release();
-            camera = null;
-        }
+//        if (camera != null) {
+//            camera.setPreviewCallback(null);
+//            camera.stopPreview();
+//            camera.release();
+//            camera = null;
+//        }
     }
 
     public void onBackClicked(View view) {
@@ -64,8 +59,8 @@ public class ProtectedBankCards extends AppCompatActivity {
     private class onAddNewCardListener implements View.OnClickListener {
         @Override
         public void onClick(View v) {
-            camera.startPreview();
-            void takePicture(Camera.ShutterCalback shutter, Camera.PictureCallback raw, Camera.PictureCallback postview, Camera.PictureCallback jpg);
+//            camera.startPreview();
+            //           void takePicture(Camera.ShutterCalback shutter, Camera.PictureCallback raw, Camera.PictureCallback postview, Camera.PictureCallback jpg);
         }
     }
 }
